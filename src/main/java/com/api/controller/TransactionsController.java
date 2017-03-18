e52447dab0292e72ec5d124a63ccfa31713b7718
Lucia <lupasarin@gmail.com>
2017-03-19 00:35:20 +0100

    Tests for TransactionsController class

--- src/main/java/com/api/controller/TransactionsController.java
+++ src/main/java/com/api/controller/TransactionsController.java
@@ -4,6 +4,7 @@ import com.api.model.Transaction;
 import com.api.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/transactions")
+@Validated
 public class TransactionsController {
     @Autowired
     private StatisticsService statisticsService;
--- src/test/java/com/api/controller/TransactionsControllerTest.java
+++ src/test/java/com/api/controller/TransactionsControllerTest.java
@@ -1,11 +1,15 @@
 package com.api.controller;
 
 import com.api.model.Transaction;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,9 +18,21 @@ public class TransactionsControllerTest {
     private TransactionsController controller;
 
     @Test
-    public void postTransactionWithNoErrors() throws Exception {
+    public void postTransactionWithNoErrors() {
         Transaction transaction = new Transaction(7890.23, System.currentTimeMillis());
+        ResponseEntity<Transaction> response = controller.postTransaction(transaction);
+        Assertions.assertThat(response).isEqualTo(ResponseEntity.noContent().build());
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void postTransactionWithNoAmount() throws Exception {
+        Transaction transaction = new Transaction(null, System.currentTimeMillis());
         controller.postTransaction(transaction);
     }
 
+    @Test(expected = ConstraintViolationException.class)
+    public void postTransactionWithNoTimestamp() throws Exception {
+        Transaction transaction = new Transaction(7890.23, null);
+        controller.postTransaction(transaction);
+    }
 }
