a8ad5b3f207da7d0833ce40e0a06da5c39a7de9a
Lucia <lupasarin@gmail.com>
2017-03-18 01:46:54 +0100

    Introduce new Config class

---
+++ src/main/java/com/api/Config.java
@@ -0,0 +1,5 @@
+package com.api;
+
+public class Config {
+    public static final int TIME_SPAN = 60000; // in milliseconds
+}
--- src/main/java/com/api/service/StatisticsService.java
+++ src/main/java/com/api/service/StatisticsService.java
@@ -1,5 +1,6 @@
 package com.api.service;
 
+import com.api.Config;
 import com.api.model.Statistics;
 import com.api.model.Transaction;
 import lombok.Getter;
@@ -12,13 +13,12 @@ public class StatisticsService {
     private Statistics statistics = new Statistics(); // TODO: synchronized if needed
 
     public void addTransaction(Transaction transaction) {
-        if (transaction.getTimestamp() >= System.currentTimeMillis() - 60000) {
-            // TODO: Make 60000 a constant in Config class or so
+        if (transaction.getTimestamp() >= System.currentTimeMillis() - Config.TIME_SPAN) {
             statistics.updateValue(transaction.getAmount());
         }
     }
 
-    @Scheduled(fixedRate = 60000) // TODO: Make this every 60 sec a constant in a Config class or so
+    @Scheduled(fixedRate = Config.TIME_SPAN)
     public void run() {
         statistics.reset();
     }
